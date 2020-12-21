f387fa70b18783773300e870888cf75aaf0ceac6
Geralt555 <70748374+Geralt555@users.noreply.github.com>
2020-12-21 11:35:07 +0300

    Add files via upload

--- ind1.py
+++ ind1.py
@@ -1,16 +1,37 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Ввести список А из 10 элементов, найти разность положительных элементов, и вывести ее
-# на экран.
+import sys
 
-# import sys
+# Ввести список А из 10 элементов, найти разность положительных элементов, и вывести ее
+# на экран. и причём надо,чтобы вычитания были попарно,2 числа и из двух ищется разница
 
 if __name__ == '__main__':
-    tuple = (1, 4, -5, -6, -9, 5, 8, -11, 13)
-    print(tuple)
-    negatives = [i for i in tuple if i < 0]
-    mul = 1
-    for i in negatives:
-        mul *= i
-    print('Произведение отрицательных: ', mul)
+    lst = tuple(map(int, input().split(" ")))
+    new_lst = ()
+
+    # Уберем все отрицательные значения
+    for item in lst:
+        if item > 0:
+            new_lst = new_lst + (item,)
+
+    # Исключим возможность нечетного количества положительных элементов
+    if len(new_lst) % 2 != 0:
+        new_lst = tuple(new_lst[:len(new_lst) - 1])
+
+    res = ()
+    skip = False
+    for i, item in enumerate(new_lst):
+        if skip:
+            skip = False
+            continue
+        if i < len(new_lst) - 1:
+            res = res + (new_lst[i] - new_lst[i + 1], )
+            skip = True
+
+    print(new_lst, res)
+
+    # tuples = [tuple(k) for k in zip(lst[::2], lst[1::2])]
+    # print(*tuples)
+    # result = [i - j for i, j in tuples]
+    # print(*result)
